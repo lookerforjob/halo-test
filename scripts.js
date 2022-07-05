98949883317ccad410026b850f9aef25482c95d0
lookerforjob <108605552+lookerforjob@users.noreply.github.com>
2022-07-05 01:30:41 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//Menu toggle button Script
 const btnMobile = document.getElementById("btn-mobile");
 
 function toggleMenu() {
@@ -6,3 +7,52 @@ function toggleMenu() {
 }
 
 btnMobile.addEventListener("click", toggleMenu);
+
+//Button go up Script
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+//Slider Js
+
+const slider= document.querySelector("#slider");
+      let sliderSection = document.querySelectorAll(".slider__section");
+      sliderSectionLast = sliderSection[sliderSection.length -1];
+
+      const btnLeft = document.querySelector("#btn-left");
+      const btnRight = document.querySelector("#btn-right");
+
+      slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+      function Next() {
+        let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+        slider.style.marginLeft = "-200%";
+        slider.style.transition = "all 0.5s";
+        setTimeout (function(){
+          slider.style.transition = "none";
+          slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+          slider.style.marginLeft = "-100%";}, 500);
+        }
+
+        btnRight.addEventListener('click', function(){
+          Next();
+        });
